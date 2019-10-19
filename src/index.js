257d7090f5250653ccd17c98a6289d9dcd8854fe
Ekaterina Tebenkova <biruta78@mail.ru>
2019-10-19 20:11:59 +0500

    feat: task ready

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let encodedWord = [];
+        let morseCode = [];
+        if (expr[i] === '*') {
+            result.push(' ');
+            continue;
+        }
+        
+        for (let a = i; a < i + 10; a++) {
+            encodedWord.push(expr[a]);
+        }
+
+        for (let b = 0; b < 10; b += 2) {
+            if (encodedWord[b] === '1' && encodedWord[b + 1] === '0'){
+                morseCode.push('.');
+            }
+            if (encodedWord[b] === '1' && encodedWord[b + 1] === '1'){
+                morseCode.push('-');
+            }
+        }
+
+        for (let [code, letter] of Object.entries(MORSE_TABLE)) {
+            if (code === morseCode.join('')) {
+                result.push(letter);
+                break;
+            }
+        }
+    }
+    return result.join('');
+
 }
 
 module.exports = {
